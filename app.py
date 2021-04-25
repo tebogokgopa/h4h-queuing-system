34f8b41b1d99817b12dd39db83553464c22454f0
williammabotjadev <williammabotjadev@gmail.com>
2021-04-25 12:32:40 +0200

    del db config

--- app.py
+++ app.py
@@ -27,54 +27,6 @@ app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'u8sAAN1FngnOJzKp-fME8NpDUfFOm65r3XmYKWjw3Vs'
 
-"""
-port = int(3306)
-
-db_url = "mysql://sql4407863:qEhcLAiTcl@https://sql4.freemysqlhosting.net:{}/sql4407863".format(port)
-
-app.config['SQLALCHEMY_DATABASE_URI'] = db_url
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", False)
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
-
-db = SQLAlchemy(app)
-
-#db.create_all()
-
-csrf = CSRFProtect(app)
-
-login_manager = LoginManager()
-login_manager.session_protection = 'strong'
-login_manager.login_view = 'login'
-
-login_manager.init_app(app)
-
-class User(UserMixin, db.Model):
-    __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key=True)
-    firstname = db.Column(db.String(80), unique=False, nullable=False)
-    lastname = db.Column(db.String(120), unique=False, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-
-    password_hash = db.Column(db.String(128))
-
-    @property
-    def password(self):
-        raise AttributeError('password is not a readable attribute')
-
-    @password.setter
-    def password(self, password):
-        self.password_hash = generate_password_hash(password)
-
-    def verify_password(self, password):
-        return check_password_hash(self.password_hash, password)
-
-    def __repr__(self):
-        return '<User %r>' % self.firstname
-
-@login_manager.user_loader
-def load_user(id):
-    return User.query.get(int(id))"""
 
 @app.route('/', methods=["GET", "POST"])
 
