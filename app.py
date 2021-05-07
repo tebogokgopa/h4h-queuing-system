0dfb58823e622f80a836bc6a4a9982857202e12d
williammabotjadev <71523683+williammabotjadev@users.noreply.github.com>
2021-05-07 13:52:12 +0200

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
 
