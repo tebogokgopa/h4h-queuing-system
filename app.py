b493a46ccff248456d64fb1ea13940bd69f9a7b0
williammabotjadev <71523683+williammabotjadev@users.noreply.github.com>
2021-05-21 13:25:17 +0200

    Update app.py

--- app.py
+++ app.py
@@ -13,16 +13,6 @@ app.config['SESSION_PERMANENT']= False
 app.config['FLASK_APP'] = os.environ.get('FLASK_APP')
 app.config['FLASK_ENV'] = os.environ.get('FLASK_ENV')
 
-class User(db.Model):
-    user_id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-
-    def __repr__(self):
-        return '<User %r>' % self.username
-
-
-
 Session(app)
 
 
